6041366238c26e9ad0abf9382b1671fafcc125f5
dineshtgiri <dineshtgiri@gmail.com>
2023-04-19 23:31:40 +0530

    first commit

---
+++ app/site/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+site = Blueprint('site', __name__, template_folder='site_templates')
+
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/user_cars')
+def user_cars():
+    return render_template('user_cars.html')
